9472bcbbcb3e114921da4a484d8d6d0520f83557
Novandev <donovan.adams@students.makeschool.com>
2019-02-16 21:06:02 -0800

    added more to the tries transferring this over the the cs21 folder

--- triesPractice.py
+++ triesPractice.py
@@ -1,8 +1,11 @@
 
-
-class Trie(object):
+# import pprint
+# pp = pprint.PrettyPrinter(indent=4)
+class Trie:
     def __init__(self):
-        self.head = {}
+        return
+
+    head = {}
                 # this represents the starting point for searching, adding, or removing words or sentences,
                 # it store a reference to the starting letter to track down ( in later methods like add or search)
                 # each letter that is seen will hold a dictionary of letters that reference other letters,
@@ -14,9 +17,41 @@ class Trie(object):
         """
         This add function takes pythons iterating over a list of keys as the for loop that it checks for each letter in the word trees dictionary
         For out first example, let's add the word "add" to the Trie
+        I'd first have to define the Trie as an empty dictionary as defined as self.head being  {}
 
-        First we define a variable curr
-          # example if I've only added the words "add", addiction" and "apple","apply" the resulting dictioanry would look like
+                        testTree = Trie()
+
+
+
+
+        Adding "add" to the Trie
+         We're going to drop in the string "add" to the Trie via the Trie.add() method
+                        Test
+
+        First we define a variable curr as the self.head so we can "creep down the tree" att every level and check its keys
+
+        lets start at the letter "a" for the forth coming for loop
+        It checks to see if "a" is a key at the first level, which would be the self.head at this point and is empty
+        We would then add "a" to the top level self.head dictionary resulting in the following
+                        self.head = {
+                                    "a"
+                                }
+        What we also need to do is give the "a" key a value that we can store letters that come after it in the word
+        In this case another empty dictionary
+
+                        self.head = {
+
+                                    "a":{}
+                                    }
+
+        We will now set "a"'s dictionary as the current value we'll check keys.
+        The next  iteration of the for loop now puts us at the letter "d"
+                and puts the curr that were on to "a"s empty dictionary
+
+                        "a":{}
+
+
+          # example if I've only added the words "add", addiction" and "apple","apply" the resulting dictionary would look like
 
                     # "a" : {
     #                           "d": {
@@ -68,6 +103,22 @@ class Trie(object):
                 curr[ch] = {}   # we add it as another  dictionary
                 print(curr)  # Check to see the current dictionary were on and what keys it has
             curr = curr[ch]  #  Either way check for the next iteration of the for loop it wil now reference
+        curr["*"]= True
+
+
+    def search(self, search_word):
+        if len(self.head) ==0:
+            raise ValueError(" The dictionary is empty, please ass words via the add method")
+
+        curr = self.head
+        for char in search_word:
+            if char not in curr:
+                return False # this will short circut and return early if, for this iteration, the character is not in the keys
+            curr = curr[char]   # if it is in the keys then for the nest letter its curr will be a list of keys that it was found
+                                # having dropped into its dictionary via the add method
 
 
 if __name__ == "__main__":
+        trieTest = Trie()
+        print(trieTest.head)
+        trieTest.search("word")
